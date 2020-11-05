733eccaaea8e5e870f61dde8aad4062d7e5d9efe
George Papadimitriou <georgpap@isi.edu>
2020-11-04 23:40:02 -0800

    added click options

--- Sites.py
+++ Sites.py
@@ -1,32 +1,45 @@
 #!/usr/bin/env python3
 
 import os
+import click
 from enum import Enum, unique
 from Pegasus.api import *
 
 @unique
 class SitesAvailable(Enum):
-    LOCAL = 1
-    USC_HPCC = 2
-    OSG_ISI = 3
-    SLURM_DEFAULT = 4
-    LSF_DEFAULT = 5
-    SUMMIT_GLITE = 6
-    SUMMIT_KUBERNETES = 7
+    LOCAL_CONDOR = 1
+    SLURM = 2
+    LSF = 3
+    SUMMIT_GLITE = 4
+    SUMMIT_KUBERNETES = 5
 
 
-SitesRequireQueue = [ SitesAvailable.SLURM_DEFAULT,
-                     SitesAvailable.LSF_DEFAULT ]
+SitesRequireQueue = [ 
+    SitesAvailable.SLURM,
+    SitesAvailable.LSF 
+]
 
 
-SitesRequireProject = [ SitesAvailable.SLURM_DEFAULT,
-                        SitesAvailable.LSF_DEFAULT,
-                        SitesAvailable.SUMMIT_GLITE, 
-                        SitesAvailable.SUMMIT_KUBERNETES ]
+SitesRequirePegasusHome = [ 
+    SitesAvailable.SLURM,
+    SitesAvailable.LSF
+]
+
+
+SitesMayRequireProject = [
+    SitesAvailable.SLURM,
+    SitesAvailable.LSF,
+]
+
+
+SitesRequireProject = [
+    SitesAvailable.SUMMIT_GLITE, 
+    SitesAvailable.SUMMIT_KUBERNETES
+]
 
 
 class MySite():
-    def __init__(self, scratch_parent_dir, storage_parent_dir, target_site:SitesAvailable, project_name="", queue_name=""):
+    def __init__(self, scratch_parent_dir, storage_parent_dir, target_site:SitesAvailable, project_name="", queue_name="", pegasus_home=""):
         self.shared_scratch_parent_dir = scratch_parent_dir
         self.local_storage_parent_dir = storage_parent_dir
 
@@ -37,27 +50,21 @@ class MySite():
                         Directory(Directory.SHARED_SCRATCH, os.path.join(self.shared_scratch_parent_dir, "scratch"))
                             .add_file_servers(FileServer("file://" + os.path.join(self.shared_scratch_parent_dir, "scratch"), Operation.ALL)),
 
-                        Directory(Directory.LOCAL_STORAGE, os.path.join(self.local_storage_parent_dir, "storage"))
-                            .add_file_servers(FileServer("file://" + os.path.join(self.local_storage_parent_dir, "storage"), Operation.ALL))
+                        Directory(Directory.LOCAL_STORAGE, os.path.join(self.local_storage_parent_dir, "output"))
+                            .add_file_servers(FileServer("file://" + os.path.join(self.local_storage_parent_dir, "output"), Operation.ALL))
                     )
 
         self.sc.add_sites(local)
         
-        if target_site is SitesAvailable.LOCAL:
+        if target_site is SitesAvailable.LOCAL_CONDOR:
             self.exec_site_name = "condorpool"
             self.condorpool()
-        elif target_site is SitesAvailable.USC_HPCC:
-            self.exec_site_name = "usc-hpcc"
-            self.usc_hpcc("quick")
-        elif target_site is SitesAvailable.OSG_ISI:
-            self.exec_site_name = "osg-isi"
-            self.osg_isi()
-        elif target_site is SitesAvailable.SLURM_DEFAULT:
-            self.exec_site_name = "slurm_default"
-            self.slurm_default(project_name, queue_name)
-        elif target_site is SitesAvailable.LSF_DEFAULT:
-            self.exec_site_name = "lsf_default"
-            self.lsf_default(project_name, queue_name)
+        elif target_site is SitesAvailable.SLURM:
+            self.exec_site_name = "slurm"
+            self.slurm(project_name, queue_name, pegasus_home)
+        elif target_site is SitesAvailable.LSF:
+            self.exec_site_name = "lsf"
+            self.lsf(project_name, queue_name, pegasus_home)
         elif target_site is SitesAvailable.SUMMIT_GLITE:
             self.exec_site_name = "summit"
             self.summit_glite(project_name, "batch")
@@ -85,26 +92,6 @@ class MySite():
         self.sc.add_sites(condorpool)
 
 
-    def usc_hpcc(self, queue_name):
-        usc = Site(self.exec_site_name)\
-                    .add_directories(
-                        Directory(Directory.SHARED_SCRATCH, os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"))
-                            .add_file_servers(FileServer("file://" + os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"), Operation.ALL))
-                    )\
-                    .add_condor_profile(grid_resource="batch slurm")\
-                    .add_pegasus_profile(
-                        style="glite",
-                        queue=queue_name,
-                        data_configuration="sharedfs",
-                        auxillary_local=True,
-                        clusters_num=2,
-                        job_aggregator="mpiexec"
-                    )\
-                    .add_env(key="PEGASUS_HOME", value="/home/rcf-proj/gmj/pegasus/SOFTWARE/pegasus/default")
-        
-        self.sc.add_sites(usc)
-
-
     def osg_isi(self):
         osg = Site(self.exec_site_name)\
                 .add_pegasus_profile(
@@ -122,7 +109,7 @@ class MySite():
         self.sc.add_sites(osg)
 
 
-    def slurm_default(self, project_name, queue_name):
+    def slurm(self, project_name, queue_name, pegasus_home):
         slurm = Site(self.exec_site_name)\
                     .add_directories(
                         Directory(Directory.SHARED_SCRATCH, os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"))
@@ -133,18 +120,23 @@ class MySite():
                         style="glite",
                         queue=queue_name,
                         data_configuration="sharedfs",
-                        auxillary_local="true",
+                        auxillary_local=True,
                         nodes=1,
                         ppn=1,
-                        project=project_name,
                         runtime=1800,
                         clusters_num=2
                     )
         
+        if project_name:
+            slurm.add_pegasus_profile(project=project_name)
+
+        if pegasus_home:
+            slurm.add_env(key="PEGASUS_HOME", value=pegasus_home)
+
         self.sc.add_sites(slurm)
 
 
-    def lsf_default(self, project_name, queue_name):
+    def lsf(self, project_name, queue_name):
         lsf = Site(self.exec_site_name)\
                     .add_directories(
                         Directory(Directory.SHARED_SCRATCH, os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"))
@@ -155,13 +147,18 @@ class MySite():
                         style="glite",
                         queue=queue_name,
                         data_configuration="sharedfs",
-                        auxillary_local="true",
+                        auxillary_local=True,
                         nodes=1,
-                        project=project_name,
                         runtime=1800,
                         clusters_num=2
                     )
+
+        if project_name:
+            lsf.add_pegasus_profile(project=project_name)
         
+        if pegasus_home:
+            lsf.add_env(key="PEGASUS_HOME", value=pegasus_home)
+
         self.sc.add_sites(lsf)
                         
 
@@ -176,7 +173,7 @@ class MySite():
                         style="glite",
                         queue=queue_name,
                         data_configuration="sharedfs",
-                        auxillary_local="true",
+                        auxillary_local=True,
                         nodes=1,
                         project=project_name,
                         job_aggregator="mpiexec",
@@ -201,8 +198,8 @@ class MySite():
                     .add_pegasus_profile(
                         style="ssh",
                         queue=queue_name,
-                        auxillary_local="true",
-                        change_dir="true",
+                        auxillary_local=True,
+                        change_dir=True,
                         nodes=1,
                         project=project_name,
                         job_aggregator="mpiexec",
@@ -214,3 +211,73 @@ class MySite():
         self.sc.add_sites(summit)
                         
 
+#### Support standalone invocation ####
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option(
+    "--execution-site",
+    type=click.Choice(SitesAvailable.__members__, case_sensitive=True),
+    prompt="What's the targeted execution site",
+    help="Target execution site"
+)
+@click.option(
+    "--project-name",
+    default="",
+    show_default=True,
+    help="Project allocation"
+)
+@click.option(
+    "--queue-name",
+    default="",
+    help="Target execution site's queue"
+)
+@click.option(
+    "--pegasus-home",
+    default="",
+    show_default=True,
+    help="Pegasus home directory location"
+)
+@click.option(
+    "--scratch-parent-dir",
+    default=os.getcwd(),
+    show_default=True,
+    help="Parent directory of scratch folder"
+)
+@click.option(
+    "--storage-parent-dir",
+    default=os.getcwd(),
+    show_default=True,
+    help="Parent directory of output folder"
+)
+
+def main(scratch_parent_dir, storage_parent_dir, execution_site, project_name, queue_name, pegasus_home):
+    execution_site = SitesAvailable[execution_site]
+    
+    if execution_site in SitesRequireQueue and queue_name == "":
+        queue_name = click.prompt("What's the execution site's queue")
+    
+    if project_name == "":
+        if execution_site in SitesRequireProject:
+            project_name = click.prompt("What's your project's name")
+        if execution_site in SitesMayRequireProject:
+            project_name = click.prompt("What's your project's name", default="", show_default=True)
+    
+    if execution_site in SitesRequirePegasusHome and pegasus_home == "":
+        pegasus_home = click.prompt("What's the location of the PEGASUS_HOME dir", default="", show_default=True)
+    
+    click.echo("Generating a Pegasus site catalog for {}".format(execution_site))
+    if queue_name:
+        click.echo("The site catalog will target queue \"{}\"".format(queue_name))
+
+    if project_name:
+        click.echo("The project allocation used is \"{}\"".format(project_name))
+    
+    if pegasus_home:
+        click.echo("The PEGASUS_HOME location is \"{}\"".format(pegasus_home))
+    
+    exec_site = MySite(scratch_parent_dir, storage_parent_dir, execution_site, project_name=project_name, queue_name=queue_name, pegasus_home=pegasus_home)
+    exec_site.write()
+
+if __name__ == "__main__":
+    main()
