0fb7e541c6affa275c55265722254f20016eef36
Karan Vahi <vahi@isi.edu>
2025-09-16 14:33:26 -0500

    for #pegasus-isi/pegasus#2125 distinguish remote scratch direcory from the scratch directory on the submit host where pegasus-init is run.

--- Sites.py
+++ Sites.py
@@ -5,6 +5,7 @@ import click
 from enum import Enum, unique
 from Pegasus.api import *
 
+
 @unique
 class SitesAvailable(Enum):
     CONDORPOOL = 1
@@ -26,16 +27,14 @@ SitesAvailableDescription = {
     SitesAvailable.SUMMIT_KUBERNETES: "OLCF Summit from OLCF Hosted Kubernetes Pod"
 }
 
-
-SitesRequireQueue = [ 
+SitesRequireQueue = [
     SitesAvailable.SLURM,
     SitesAvailable.REMOTE_SLURM,
     SitesAvailable.LSF,
     SitesAvailable.SGE
 ]
 
-
-SitesRequirePegasusHome = [ 
+SitesRequirePegasusHome = [
     SitesAvailable.SLURM,
     SitesAvailable.REMOTE_SLURM,
     SitesAvailable.LSF,
@@ -49,17 +48,12 @@ SitesMayRequireProject = [
     SitesAvailable.SGE
 ]
 
-
 SitesRequireProject = [
-    SitesAvailable.SUMMIT_GLITE, 
+    SitesAvailable.SUMMIT_GLITE,
     SitesAvailable.SUMMIT_KUBERNETES
 ]
 
 SitesRequireScratch = [
-    SitesAvailable.REMOTE_SLURM
-]
-
-SitesRequireStorage = [
     SitesAvailable.REMOTE_SLURM
 ]
 
@@ -69,24 +63,29 @@ SitesAreRemote = [
 
 
 class MySite():
-    def __init__(self, scratch_parent_dir, storage_parent_dir, target_site:SitesAvailable, project_name="", queue_name="", pegasus_home="", login_host="",
-        transfer_endpoint=""):
+    def __init__(self, scratch_parent_dir, storage_parent_dir, target_site: SitesAvailable, project_name="",
+                 queue_name="", pegasus_home="", login_host="",
+                 transfer_endpoint="",
+                 remote_shared_scratch_parent_dir=""):
+
         self.shared_scratch_parent_dir = scratch_parent_dir
         self.local_storage_parent_dir = storage_parent_dir
 
         self.sc = SiteCatalog()
 
-        local = Site("local")\
-                    .add_directories(
-                        Directory(Directory.SHARED_SCRATCH, os.path.join(self.shared_scratch_parent_dir, "scratch"))
-                            .add_file_servers(FileServer("file://" + os.path.join(self.shared_scratch_parent_dir, "scratch"), Operation.ALL)),
+        local = Site("local") \
+            .add_directories(
+            Directory(Directory.SHARED_SCRATCH, os.path.join(self.shared_scratch_parent_dir, "scratch"))
+            .add_file_servers(
+                FileServer("file://" + os.path.join(self.shared_scratch_parent_dir, "scratch"), Operation.ALL)),
 
-                        Directory(Directory.LOCAL_STORAGE, os.path.join(self.local_storage_parent_dir, "output"))
-                            .add_file_servers(FileServer("file://" + os.path.join(self.local_storage_parent_dir, "output"), Operation.ALL))
-                    )
+            Directory(Directory.LOCAL_STORAGE, os.path.join(self.local_storage_parent_dir, "output"))
+            .add_file_servers(
+                FileServer("file://" + os.path.join(self.local_storage_parent_dir, "output"), Operation.ALL))
+        )
 
         self.sc.add_sites(local)
-        
+
         self.exec_site_arch = None
         if target_site is SitesAvailable.CONDORPOOL:
             self.exec_site_name = "condorpool"
@@ -96,7 +95,8 @@ class MySite():
             self.slurm(project_name, queue_name, pegasus_home)
         elif target_site is SitesAvailable.REMOTE_SLURM:
             self.exec_site_name = "remote_slurm"
-            self.remote_slurm(project_name, queue_name, pegasus_home, login_host, transfer_endpoint)
+            self.remote_slurm(project_name, queue_name, pegasus_home, login_host, transfer_endpoint,
+                              remote_shared_scratch_parent_dir)
         elif target_site is SitesAvailable.LSF:
             self.exec_site_name = "lsf"
             self.lsf(project_name, queue_name, pegasus_home)
@@ -112,61 +112,60 @@ class MySite():
             self.exec_site_arch = Arch.PPC64LE
             self.summit_kubernetes(project_name, "batch")
 
-
     def write(self):
         self.sc.write()
 
-
     def condorpool(self):
-        condorpool = Site(self.exec_site_name)\
-                        .add_pegasus_profile(
-                            style="condor",
-                            data_configuration="condorio",
-                            clusters_num=2
-                        )\
-                        .add_condor_profile(
-                            universe="vanilla",
-                            periodic_remove="(JobStatus == 5) && ((CurrentTime - EnteredCurrentStatus) > 10)"
-                        )
+        condorpool = Site(self.exec_site_name) \
+            .add_pegasus_profile(
+            style="condor",
+            data_configuration="condorio",
+            clusters_num=2
+        ) \
+            .add_condor_profile(
+            universe="vanilla",
+            periodic_remove="(JobStatus == 5) && ((CurrentTime - EnteredCurrentStatus) > 10)"
+        )
 
         self.sc.add_sites(condorpool)
 
-
     def osg_isi(self):
-        osg = Site(self.exec_site_name)\
-                .add_pegasus_profile(
-                    style="condor",
-                    data_configuration="condorio",
-                    clusters_num=2
-                )\
-                .add_condor_profile(
-                    universe="vanilla",
-                    periodic_remove="(JobStatus == 5) && ((CurrentTime - EnteredCurrentStatus) > 10)",
-                    requirements="OSGVO_OS_STRING == \"RHEL 6\" && Arch == \"X86_64\" &&  HAS_MODULES == True"
-                )\
-                .add_profiles(Namespace.CONDOR, key="+ProjectName", value="PegasusTraining")
+        osg = Site(self.exec_site_name) \
+            .add_pegasus_profile(
+            style="condor",
+            data_configuration="condorio",
+            clusters_num=2
+        ) \
+            .add_condor_profile(
+            universe="vanilla",
+            periodic_remove="(JobStatus == 5) && ((CurrentTime - EnteredCurrentStatus) > 10)",
+            requirements="OSGVO_OS_STRING == \"RHEL 6\" && Arch == \"X86_64\" &&  HAS_MODULES == True"
+        ) \
+            .add_profiles(Namespace.CONDOR, key="+ProjectName", value="PegasusTraining")
 
         self.sc.add_sites(osg)
 
-
     def slurm(self, project_name, queue_name, pegasus_home):
-        slurm = Site(self.exec_site_name)\
-                    .add_directories(
-                        Directory(Directory.SHARED_SCRATCH, os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"))
-                            .add_file_servers(FileServer("file://" + os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"), Operation.ALL))
-                    )\
-                    .add_condor_profile(grid_resource="batch slurm")\
-                    .add_pegasus_profile(
-                        style="glite",
-                        queue=queue_name,
-                        data_configuration="sharedfs",
-                        auxillary_local="true",
-                        nodes=1,
-                        ppn=1,
-                        runtime=1800,
-                        clusters_num=2
-                    )
-        
+        slurm = Site(self.exec_site_name) \
+            .add_directories(
+            Directory(Directory.SHARED_SCRATCH,
+                      os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"))
+            .add_file_servers(
+                FileServer("file://" + os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"),
+                           Operation.ALL))
+        ) \
+            .add_condor_profile(grid_resource="batch slurm") \
+            .add_pegasus_profile(
+            style="glite",
+            queue=queue_name,
+            data_configuration="sharedfs",
+            auxillary_local="true",
+            nodes=1,
+            ppn=1,
+            runtime=1800,
+            clusters_num=2
+        )
+
         if project_name:
             slurm.add_pegasus_profile(project=project_name)
 
@@ -175,7 +174,8 @@ class MySite():
 
         self.sc.add_sites(slurm)
 
-    def remote_slurm(self, project_name, queue_name, pegasus_home, login_host="", transfer_endpoint=""):
+    def remote_slurm(self, project_name, queue_name, pegasus_home, login_host="", transfer_endpoint="",
+                     remote_shared_scratch_parent_dir=""):
         if not transfer_endpoint:
             transfer_endpoint = "file://"
 
@@ -184,17 +184,19 @@ class MySite():
         slurm = (Site(self.exec_site_name) \
             .add_directories(
             Directory(Directory.SHARED_SCRATCH,
-                      os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"))
+                      os.path.join(remote_shared_scratch_parent_dir, self.exec_site_name, "scratch"))
             .add_file_servers(
                 FileServer(
-                    transfer_endpoint + os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"),
+                    transfer_endpoint + os.path.join(remote_shared_scratch_parent_dir, self.exec_site_name, "scratch"),
                     Operation.ALL))
         ) \
             .add_grids(
-            Grid(grid_type=Grid.BATCH, scheduler_type=Scheduler.SLURM, contact=login_host, job_type=SupportedJobs.COMPUTE)
+            Grid(grid_type=Grid.BATCH, scheduler_type=Scheduler.SLURM, contact=login_host,
+                 job_type=SupportedJobs.COMPUTE)
         )
             .add_grids(
-            Grid(grid_type=Grid.BATCH, scheduler_type=Scheduler.SLURM, contact=login_host, job_type=SupportedJobs.AUXILLARY)
+            Grid(grid_type=Grid.BATCH, scheduler_type=Scheduler.SLURM, contact=login_host,
+                 job_type=SupportedJobs.AUXILLARY)
         )
             .add_pegasus_profile(
             style="ssh",
@@ -216,26 +218,29 @@ class MySite():
         self.sc.add_sites(slurm)
 
     def lsf(self, project_name, queue_name, pegasus_home):
-        lsf = Site(self.exec_site_name)\
-                    .add_directories(
-                        Directory(Directory.SHARED_SCRATCH, os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"))
-                            .add_file_servers(FileServer("file://" + os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"), Operation.ALL))
-                    )\
-                    .add_condor_profile(grid_resource="batch lsf")\
-                    .add_pegasus_profile(
-                        style="glite",
-                        queue=queue_name,
-                        data_configuration="sharedfs",
-                        auxillary_local="true",
-                        nodes=1,
-                        ppn=1,
-                        runtime=1800,
-                        clusters_num=2
-                    )
+        lsf = Site(self.exec_site_name) \
+            .add_directories(
+            Directory(Directory.SHARED_SCRATCH,
+                      os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"))
+            .add_file_servers(
+                FileServer("file://" + os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"),
+                           Operation.ALL))
+        ) \
+            .add_condor_profile(grid_resource="batch lsf") \
+            .add_pegasus_profile(
+            style="glite",
+            queue=queue_name,
+            data_configuration="sharedfs",
+            auxillary_local="true",
+            nodes=1,
+            ppn=1,
+            runtime=1800,
+            clusters_num=2
+        )
 
         if project_name:
             lsf.add_pegasus_profile(project=project_name)
-        
+
         if pegasus_home:
             lsf.add_env(key="PEGASUS_HOME", value=pegasus_home)
 
@@ -271,58 +276,67 @@ class MySite():
         self.sc.add_sites(sge)
 
     def summit_glite(self, project_name, queue_name):
-        summit = Site(self.exec_site_name)\
-                    .add_directories(
-                        Directory(Directory.SHARED_SCRATCH, os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"))
-                            .add_file_servers(FileServer("file://" + os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"), Operation.ALL))
-                    )\
-                    .add_condor_profile(grid_resource="batch lsf")\
-                    .add_pegasus_profile(
-                        style="glite",
-                        queue=queue_name,
-                        data_configuration="sharedfs",
-                        auxillary_local="true",
-                        nodes=1,
-                        project=project_name,
-                        job_aggregator="mpiexec",
-                        runtime=1800,
-                        clusters_num=2
-                    )\
-                    .add_env(key="PEGASUS_HOME", value="/ccs/proj/csc355/summit/pegasus/stable")
-        
+        summit = Site(self.exec_site_name) \
+            .add_directories(
+            Directory(Directory.SHARED_SCRATCH,
+                      os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"))
+            .add_file_servers(
+                FileServer("file://" + os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"),
+                           Operation.ALL))
+        ) \
+            .add_condor_profile(grid_resource="batch lsf") \
+            .add_pegasus_profile(
+            style="glite",
+            queue=queue_name,
+            data_configuration="sharedfs",
+            auxillary_local="true",
+            nodes=1,
+            project=project_name,
+            job_aggregator="mpiexec",
+            runtime=1800,
+            clusters_num=2
+        ) \
+            .add_env(key="PEGASUS_HOME", value="/ccs/proj/csc355/summit/pegasus/stable")
+
         self.sc.add_sites(summit)
-                        
 
     def summit_kubernetes(self, project_name, queue_name):
-        summit = Site(self.exec_site_name)\
-                    .add_grids(
-                        Grid(grid_type=Grid.BATCH, scheduler_type=Scheduler.LSF, contact="${USER}@dtn.ccs.ornl.gov", job_type=SupportedJobs.COMPUTE),
-                        Grid(grid_type=Grid.BATCH, scheduler_type=Scheduler.LSF, contact="${USER}@dtn.ccs.ornl.gov", job_type=SupportedJobs.AUXILLARY)
-                    )\
-                    .add_directories(
-                        Directory(Directory.SHARED_SCRATCH, os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"))
-                            .add_file_servers(FileServer("file://" + os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"), Operation.ALL))
-                    )\
-                    .add_pegasus_profile(
-                        style="ssh",
-                        queue=queue_name,
-                        auxillary_local="true",
-                        data_configuration="sharedfs",
-                        change_dir="true",
-                        nodes=1,
-                        project=project_name,
-                        job_aggregator="mpiexec",
-                        runtime=1800,
-                        clusters_num=2
-                    )\
-                    .add_env(key="PEGASUS_HOME", value="/ccs/proj/csc355/summit/pegasus/stable")
-        
+        summit = Site(self.exec_site_name) \
+            .add_grids(
+            Grid(grid_type=Grid.BATCH, scheduler_type=Scheduler.LSF, contact="${USER}@dtn.ccs.ornl.gov",
+                 job_type=SupportedJobs.COMPUTE),
+            Grid(grid_type=Grid.BATCH, scheduler_type=Scheduler.LSF, contact="${USER}@dtn.ccs.ornl.gov",
+                 job_type=SupportedJobs.AUXILLARY)
+        ) \
+            .add_directories(
+            Directory(Directory.SHARED_SCRATCH,
+                      os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"))
+            .add_file_servers(
+                FileServer("file://" + os.path.join(self.shared_scratch_parent_dir, self.exec_site_name, "scratch"),
+                           Operation.ALL))
+        ) \
+            .add_pegasus_profile(
+            style="ssh",
+            queue=queue_name,
+            auxillary_local="true",
+            data_configuration="sharedfs",
+            change_dir="true",
+            nodes=1,
+            project=project_name,
+            job_aggregator="mpiexec",
+            runtime=1800,
+            clusters_num=2
+        ) \
+            .add_env(key="PEGASUS_HOME", value="/ccs/proj/csc355/summit/pegasus/stable")
+
         self.sc.add_sites(summit)
-                        
+
 
 #### Support standalone invocation ####
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.option(
     "--execution-site",
@@ -371,28 +385,29 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
     show_default=True,
     help="transfer endpoint for the remote cluster to use for data transfers"
 )
-
-def main(scratch_parent_dir, storage_parent_dir, execution_site, project_name, queue_name, pegasus_home, login_host, transfer_endpoint):
+def main(scratch_parent_dir, storage_parent_dir, execution_site, project_name, queue_name, pegasus_home, login_host,
+         transfer_endpoint):
     execution_site = SitesAvailable[execution_site]
-    
+
     if execution_site in SitesRequireQueue and queue_name is None:
         queue_name = click.prompt("What's the execution site's queue")
-    
+
     if execution_site in SitesRequireProject and not project_name:
         project_name = click.prompt("What's your project's name")
     elif execution_site in SitesMayRequireProject and project_name is None:
         project_name = click.prompt("What's your project's name", default="", show_default=True)
-    
+
     if execution_site in SitesRequirePegasusHome and pegasus_home is None:
-        pegasus_home = click.prompt("What's the location of the PEGASUS_HOME dir on the compute nodes", default="", show_default=True)
-    
+        pegasus_home = click.prompt("What's the location of the PEGASUS_HOME dir on the compute nodes", default="",
+                                    show_default=True)
+
     click.echo("Generating a Pegasus site catalog for {}".format(execution_site))
     if queue_name:
         click.echo("The site catalog will target queue \"{}\"".format(queue_name))
 
     if project_name:
         click.echo("The project allocation used is \"{}\"".format(project_name))
-    
+
     if pegasus_home:
         click.echo("The PEGASUS_HOME location is \"{}\"".format(pegasus_home))
 
@@ -400,6 +415,7 @@ def main(scratch_parent_dir, storage_parent_dir, execution_site, project_name, q
                        queue_name=queue_name, pegasus_home=pegasus_home, login_host=login_host,
                        transfer_endpoint=transfer_endpoint)
     exec_site.write()
+
 
 if __name__ == "__main__":
     main()
